9f2f61acd9e1a086f5163c5a238cb9d38e3b7171
arunkp2513 <99520584+arunkp2513@users.noreply.github.com>
2022-05-11 22:42:27 -0700

    React-UI Fix1

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,34 @@
-import React from "react";
-<<<<<<< Updated upstream
-=======
+import React from 'react';
 import PropTypes from 'prop-types';
-import  Button  from "react-bootstrap/Button";
-import Card from 'react-bootstrap/Card'
->>>>>>> Stashed changes
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
-    render(){
-        const{movie, onMovieClick} = this.props;
-<<<<<<< Updated upstream
-        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
-=======
-        
-        return(
-            <card>
-                <Card.Img variant="top" src={movie.ImagePath} />
-                <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick = {()=> onMovieClick(movie)} variant='link'>Open</Button>
-                </Card.Body>
-            </card>
-        );
->>>>>>> Stashed changes
-    }
+  render() {
+    const { movie, onMovieClick } = this.props;
+
+    return (
+      <div
+        className="movie-card"
+        onClick={() => {
+          onMovieClick(movie);
+        }}
+      >
+        {movie.Title}
+      </div>
+    );
+
+    return (
+      <card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">
+            Open
+          </Button>
+        </Card.Body>
+      </card>
+    );
+  }
 }
