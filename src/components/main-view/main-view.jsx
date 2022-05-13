e763249e6800b3dbe72f9774d969ca43b79732e3
arunkp2513 <99520584+arunkp2513@users.noreply.github.com>
2022-05-12 20:40:21 -0700

    Minor fix

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,63 +1,71 @@
 import React from 'react'; // Not a part of a component but required to create a component.
-import axios from 'axios'; // Used to fetch movies 
+import axios from 'axios'; // Used to fetch movies
 import { MovieView } from '../movie-view/movie-view';
-import {MovieCard} from '../movie-card/movie-card';
+import { MovieCard } from '../movie-card/movie-card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// react.component is a template/blueprint for creating new components. 
+// react.component is a template/blueprint for creating new components.
 // export exposes the Mainview component which makes it available for use by other components/modules/files(import it)
 // React creates MainView component using the generic React.compnent
 export class MainView extends React.Component {
+  constructor() {
+    // constructor method is used to create component.
+    super(); // related to OOP which initilazes the component's state.
+    //This code is executed as soon as component is created.
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+    };
+  }
 
-    constructor(){ // constructor method is used to create component.
-        super(); // related to OOP which initilazes the component's state.
-        //This code is executed as soon as component is created.
-        this.state = {
-          movies: [],
-          selectedMovie : null
-        };
-     }
+  componentDidMount() {
+    axios
+      .get('https://myflix2513.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(error => {
+        console.error.log(error);
+      });
+  }
 
-     componentDidMount(){
-         axios.get('https://myflix2513.herokuapp.com/movies')
-         .then(response =>{
-             this.setState({
-                 movies: response.data
-             });
-         })
-         .catch(error => {
-             console;error.log(error);
-         });
-     }
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie,
+    });
+  }
+  render() {
+    const { movies, selectedMovie } = this.state;
 
+    if (movies.length === 0) return <div className="main-view" />;
 
-      setSelectedMovie(newSelectedMovie){   
-          this.setState({
-              selectedMovie : newSelectedMovie
-          });
-      }
-      render() {
-        const { movies, selectedMovie } = this.state;
-    
-    
-        if (movies.length === 0) return <div className="main-view" />;
-    
-        return (
-          
-            <Row className="main-view justify-content-md-center">
-              {selectedMovie
-                ? (
-                  <Col md={8}>
-                    <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                  </Col>
-                )
-                : movies.map(movie => (
-                  <Col md={3}>
-                    <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-                  </Col>
-                ))
-              }
-            </Row>
-          );
-      }
-    }
+    return (
+      <Row className="main-view justify-content-md-center">
+        {selectedMovie ? (
+          <Col md={8}>
+            <MovieView
+              movie={selectedMovie}
+              onBackClick={newSelectedMovie => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          </Col>
+        ) : (
+          movies.map(movie => (
+            <Col md={3}>
+              <MovieCard
+                key={movie._id}
+                movie={movie}
+                onMovieClick={newSelectedMovie => {
+                  this.setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            </Col>
+          ))
+        )}
+      </Row>
+    );
+  }
+}
